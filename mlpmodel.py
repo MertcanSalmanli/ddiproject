1d135a097308ed4de9787615f6847aaf7f0237c7
Mertcan Salmanlı <129759028+MertcanSalmanli@users.noreply.github.com>
2024-05-28 12:16:09 +0300

    Update mlpmodel.py

--- mlpmodel.py
+++ mlpmodel.py
@@ -1,32 +1,58 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import load_model
 import joblib
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import stanza
+from gensim.models import Word2Vec
 
-# Modeli ve label encoder'ı yükleme
-mlp_model = load_model('mlp_model.h5')
-label_encoder = joblib.load('label_encoder.pkl')
+# NLTK Türkçe stopwords ve tokenizerlar
+nltk.download('punkt')
+nltk.download('stopwords')
 
-# Streamlit arayüzü
-st.title('Haber Etiketleme Tahmini')
-input_text = st.text_area("Bir haber yazısı girin ve hangi etiketten olduğunu tahmin edin:")
+# Türkçe stopwords'ler
+stop_words = set(stopwords.words('turkish'))
 
-if st.button('Tahmin Et'):
-    if input_text:
-        # Giriş metnini vektöre dönüştürme
-        input_vector = np.array([input_text.split()])  # Basitçe metni kelimelere ayırıyoruz
-        
-        # Dönüştürülmüş girdiyi modelin beklediği forma getirme
-        input_vector = input_vector.reshape((input_vector.shape[0], -1))
-        
-        # Tahmin yapma
-        prediction = mlp_model.predict(input_vector)
-        predicted_label = np.argmax(prediction)
-        
-        # Sayısal etiketi gerçek etikete dönüştürme
-        predicted_class = label_encoder.inverse_transform([predicted_label])[0]
-        
-        # Tahmin sonucunu gösterme
-        st.write(f"Tahmin edilen etiket: {predicted_class}")
+# Stanza 
+stanza.download('tr')
+nlp = stanza.Pipeline(lang='tr', processors='tokenize,mwt,pos,lemma')
+
+# Haber metnini işlemek için bir fonksiyon
+def haber_metnini_islemle(haber_metni):
+    # Gerekli ön işlemleri yapın ve vektörleştirin
+    text = haber_metni.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'\d+', '', text)
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word not in stop_words]
+    lemmatized_tokens = []
+    for token in tokens:
+        doc = nlp(token)
+        for sent in doc.sentences:
+            for word in sent.words:
+                lemmatized_tokens.append(word.lemma)
+    # Burada vektörleştirme işlemini yapın
+    word_vectors = [model.wv[word] for word in lemmatized_tokens if word in model.wv]
+    if len(word_vectors) > 0:
+        return np.mean(word_vectors, axis=0)
     else:
-        st.write("Lütfen bir haber yazısı girin.")
+        return np.zeros(model.vector_size)
+
+# Streamlit uygulamasını oluşturma
+st.title("Haber Türü Tahmini")
+
+# Haber metnini giriş alanı
+haber_metni = st.text_area("Haber Metni", "")
+
+# Tahmin et butonu
+if st.button("Tahmin Et"):
+    # Tahmin etme işlemi
+    tahmin = haber_turunu_tahmin_et(haber_metni)
+    
+    # Tahmini sonucu kullanıcıya gösterme
+    st.write("Tahmin Edilen Haber Türü:", tahmin)
