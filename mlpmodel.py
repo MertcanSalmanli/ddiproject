62c36be0a0e85d440b9450958cadc6e81ecd978b
Mertcan Salmanlı <129759028+MertcanSalmanli@users.noreply.github.com>
2024-05-28 12:04:52 +0300

    Update mlpmodel.py

--- mlpmodel.py
+++ mlpmodel.py
@@ -1,37 +1,32 @@
-import pandas as pd
+import streamlit as st
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import load_model
 import joblib
-import streamlit as st
 
 # Modeli ve label encoder'ı yükleme
 mlp_model = load_model('mlp_model.h5')
 label_encoder = joblib.load('label_encoder.pkl')
 
-# Fonksiyonlar
+# Streamlit arayüzü
+st.title('Haber Etiketleme Tahmini')
+input_text = st.text_area("Bir haber yazısı girin ve hangi etiketten olduğunu tahmin edin:")
 
-
-
-def haber_turunu_tahmin_et(haber_metni):
-  # Metni işleme
-  islemli_metin = haber_metnini_islemle(haber_metni)
-  
-  # Kelimeleri sayısal değerlere dönüştürme
-  sayisal_metin = label_encoder.transform(islemli_metin)
-  
-  # Tahmini yapma
-  tahmin = mlp_model.predict_classes(np.array([sayisal_metin]))[0]
-  tahmin = label_encoder.inverse_transform([tahmin])[0]
-  return tahmin
-
-# Streamlit uygulaması
-
-st.title("Haber Türü Tahmini")
-
-haber_metni = st.text_input("Haber metnini giriniz:")
-
-if st.button("Tahmin Et"):
-  tahmin = haber_turunu_tahmin_et(haber_metni)
-  st.write("Tahmin edilen haber türü:", tahmin)
+if st.button('Tahmin Et'):
+    if input_text:
+        # Giriş metnini vektöre dönüştürme
+        input_vector = np.array([input_text.split()])  # Basitçe metni kelimelere ayırıyoruz
+        
+        # Dönüştürülmüş girdiyi modelin beklediği forma getirme
+        input_vector = input_vector.reshape((input_vector.shape[0], -1))
+        
+        # Tahmin yapma
+        prediction = mlp_model.predict(input_vector)
+        predicted_label = np.argmax(prediction)
+        
+        # Sayısal etiketi gerçek etikete dönüştürme
+        predicted_class = label_encoder.inverse_transform([predicted_label])[0]
+        
+        # Tahmin sonucunu gösterme
+        st.write(f"Tahmin edilen etiket: {predicted_class}")
+    else:
+        st.write("Lütfen bir haber yazısı girin.")
